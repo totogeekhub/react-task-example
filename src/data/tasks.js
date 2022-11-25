0aa06ab58082da4b96671bc282a0dde992265b23
Víctor Serrano Arriagada <victor.serrano1992@gmail.com>
2022-11-25 00:45:58 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "mi primer tarea de la descripcion XD",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "mi segunda tarea de la descripcion XD",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "mi tercera tarea de la descripcion XD",
+  },
+];
